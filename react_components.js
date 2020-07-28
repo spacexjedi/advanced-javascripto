e49b170c5e95654e31d05b6f1a3289396b61cada
spacexjedi <58277328+spacexjedi@users.noreply.github.com>
2020-07-28 19:17:54 -0300

    Create react_components.js

---
+++ react_components.js
@@ -0,0 +1,12 @@
+/* crie uma pasta chamada LogoFooter dentro de components, dentro dessa pasta crie um index.js */
+import styled from 'styled-components'
+import logo from '../../assets/img/jedi.svg'
+
+const LogoJedi = styled.img.attrs({src: logo, alt: 'logo footer'})`
+
+
+
+
+`;
+
+export default LogoJedi;
